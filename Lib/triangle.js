4f53f2b3393b6eb7c578cd9f85d9741c7142f005
Michael Mount <mount.photos@gmail.com>
2023-03-08 18:10:12 -0700

    Refactored trigale constroctor output to be full svg file and updated triangle test

--- Lib/triangle.js
+++ Lib/triangle.js
@@ -6,7 +6,7 @@ class Triangle extends Shape {
     this.shape = shape;
   }
   render() {
-    return `<polygon points="150, 18 244, 182 56, 182" fill="${this.bgColor}" />`;
+    return `<svg width="200" height="300"><polygon points="75 0, 170 200, 0 200" fill="${this.bgColor}"/><text x="35" y="130" fill="${this.textColor}" font-size="50">${this.text}</text></svg>`;
   }
 }
 
--- Lib/triangle.test.js
+++ Lib/triangle.test.js
@@ -2,10 +2,10 @@ const Triangle = require("./triangle.js");
 
 describe("Triangle", () => {
   test("should apply string for SVG triangle", () => {
-    const shape = new Triangle("test", "red", "triangle", "blue");
+    const shape = new Triangle("mjm", "red", "triangle", "blue");
 
     expect(shape.render()).toEqual(
-      '<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
+      '<svg width="200" height="300"><polygon points="75 0, 170 200, 0 200" fill="blue"/><text x="35" y="130" fill="red" font-size="50">mjm</text></svg>'
     );
   });
 });
