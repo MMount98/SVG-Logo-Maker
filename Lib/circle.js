386e3602a0a80eea7d29317b30d1988a54affb36
Michael Mount <mount.photos@gmail.com>
2023-03-08 17:58:31 -0700

    changed output of circle constructor to wrtie full SVG code

--- Lib/circle.js
+++ Lib/circle.js
@@ -6,7 +6,7 @@ class Circle extends Shape {
     this.shape = shape;
   }
   render() {
-    return `<${this.shape} cx="50" cy="50" r="40" fill="${this.bgColor}" />`;
+    return `<svg width="200" height="300"><${this.shape} cx="50" cy="50" r="40" fill="${this.bgColor}"/><text x="5" y="65" fill="${this.textColor}" font-size="50">${this.text}</text></svg>`;
   }
 }
 
--- Lib/circle.test.js
+++ Lib/circle.test.js
@@ -2,10 +2,10 @@ const Circle = require("./circle.js");
 
 describe("Circle", () => {
   test("should apply string for SVG circle", () => {
-    const shape = new Circle("test", "red", "circle", "blue");
+    const shape = new Circle("mjm", "red", "circle", "blue");
 
     expect(shape.render()).toEqual(
-      '<circle cx="50" cy="50" r="40" fill="blue" />'
+      '<svg width="200" height="300"><circle cx="50" cy="50" r="40" fill="blue"/><text x="5" y="65" fill="red" font-size="50">mjm</text></svg>'
     );
   });
 });
